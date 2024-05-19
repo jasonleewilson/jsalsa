dd7fb1ec2083a4df990147a5c08d562b6a153feb
Jason Wilson <info@jasonleewilson.com>
2024-05-19 17:10:49 -0500

    added GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <GoogleTagManager gtmId='GTM-K8BJWSMQ' />
       <body className={inter.className}>{children}</body>
     </html>
   );
